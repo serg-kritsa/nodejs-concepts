0a3b6441a6f162cd43198604c1933879d1421aa9
Serg <serg@example.com>
2021-06-12 20:32:23 +0300

    added password hashing

---
+++ node-basics/00-demo/bcrypt-demo.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = false;
+    isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+
+    isMatch = await bcrypt.compare('red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
