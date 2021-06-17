5ef53ff74ce4e7033cb30e411c7b7f41b5d2d65e
Serg <serg@example.com>
2021-06-17 23:01:17 +0300

    pass & fromat message time

---
+++ chat-app/src/date-demo.js
@@ -0,0 +1,8 @@
+const now = new Date();
+let tmp
+tmp = now.toString();
+console.log(tmp); // Thu Jun 17 2021 22:44:48 GMT+0300 (Москва, стандартное время)
+tmp = now.getDate();
+console.log(tmp); // 17
+tmp = now.getTime()
+console.log(tmp); // 1623959088025
