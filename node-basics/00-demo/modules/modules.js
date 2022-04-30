0bfe1511532cb7a28e2138783fc77be801b4131b
Serg <serg@example.com>
2022-05-01 00:02:23 +0300

    added basics demo

---
+++ node-basics/00-demo/modules/modules.js
@@ -0,0 +1,25 @@
+// (function (exports, require, module, __filename, __dirname) {
+//     // wrapper proof 1
+//     // console.log(arguments);
+    
+//     // program code 
+// });
+
+// code itself
+// console.log(require('any-module').wrapper);
+
+
+// module.exports
+const C = require("./test-module-1");
+const calc1 = new C();
+console.log(calc1.add(2, 5));
+
+// exports
+// const calc2 = require("./test-module-2");
+const { add, multiply } = require("../test-module-2");
+console.log(multiply(2, 5));
+
+// caching
+require("../test-module-3")();
+require("../test-module-3")();
+require("../test-module-3")();
